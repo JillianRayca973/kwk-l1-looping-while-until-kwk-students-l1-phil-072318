261775890bfc4cb3fe9d856c87dff5d2976145d1
Jill  Rayca <rkrayca@yahoo.com>
2018-07-24 20:01:26 +0000

    automatically backed up by learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,9 +1,10 @@
 def using_while
 	levitation_force = 6
  loop do 
-   until levitation_force==10
-   puts levitation_force += 1
-  end 
+    until levitation_force==10
+      puts levitation_force += 1
+    end 
+  end
 end
 
 using_while
